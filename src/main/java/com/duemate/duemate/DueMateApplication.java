f80d033f92f2e1cb2e7f3700993b87e98516fd50
Jason Le <jason.le0804@gmail.com>
2026-07-20 21:55:02 -0500

    move application to root

--- src/main/java/com/duemate/duemate/DueMateApplication.java
+++ src/main/java/com/duemate/duemate/DueMateApplication.java
@@ -1,4 +1,4 @@
-package com.duemate.duemate.application;
+package com.duemate.duemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
