0178f51d09dd23d34196aeec8dc7e53ad739e5c4
Jason Le <jason.le0804@gmail.com>
2026-07-20 22:04:13 -0500

    add @RequestBody to UserController POST method

--- src/main/java/com/duemate/duemate/controller/UserController.java
+++ src/main/java/com/duemate/duemate/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class UserController {
 
     // POST - Create a user
     @PostMapping
-    public User createUser(User user) {
+    public User createUser(@RequestBody User user) {
         return userService.createUser(user.getEmail(), user.getPassword(), user.getDefaultCurrency());
     }
 
